2906dbad73cdad749b0774ec6b1bf95b01fd523d
Sylfvain <sureapa243@g.educaand.es>
2024-04-19 12:41:55 +0200

    Base datos corregida

--- ProyectoCooperativa/Base de Datos/Base de Datos Final.sql
+++ ProyectoCooperativa/Base de Datos/Base de Datos Final.sql
@@ -21,8 +21,14 @@ CREATE TABLE SOCIO (
 CREATE TABLE  VENTAS (
   N_Ventas integer AUTO_INCREMENT,
   Fecha_y_hora timestamp NOT NULL,
+  NIF VARCHAR(9) NOT NULL,
 
-  CONSTRAINT VENTAS_pk PRIMARY KEY(N_Ventas)
+  CONSTRAINT VENTAS_pk PRIMARY KEY(N_Ventas),
+  CONSTRAINT VENTAS_socio_fk FOREIGN KEY (NIF)
+                            REFERENCES SOCIO(NIF)
+                            ON UPDATE CASCADE
+                            ON DELETE CASCADE
+
 );
 
 
@@ -33,9 +39,14 @@ CREATE TABLE ENTREGA (
   Tipo_aceituna ENUM('Árbol','Suelo') NOT NULL,
   Parcela_SIGPAC int NOT NULL,
   Recinto_SIGPAC int NOT NULL,
+   NIF VARCHAR(9) NOT NULL,
   
   CONSTRAINT ENTREGA_pk PRIMARY KEY(N_Entrega),
-  CONSTRAINT Entrega_Cant_ck CHECK (Cantidad>0)
+  CONSTRAINT Entrega_Cant_ck CHECK (Cantidad>0),
+  CONSTRAINT Entrega_socio_fk FOREIGN KEY (NIF)
+                              REFERENCES SOCIO(NIF)
+                              ON UPDATE CASCADE
+                              ON DELETE CASCADE
 );
 
 CREATE TABLE  PRODUCTO (
@@ -47,53 +58,22 @@ CREATE TABLE  PRODUCTO (
   CONSTRAINT PRODUCTO_Precio_ck CHECK (Precio>0)
 );
 
--- Tablas intermedias de Socio y Venta, Socio y Entrega y Entrega y Producto.
+-- Tablas intermedias de Venta y Producto.
 
-CREATE TABLE SOCIO_VENTAS (
-  NIF VARCHAR(9) NOT NULL,
+
+CREATE TABLE VENTAS_PRODUCTO (
   N_Ventas int NOT NULL,
-  
-  CONSTRAINT SOCIO_VENTAS_pk PRIMARY KEY(NIF,N_Ventas),
-
-  CONSTRAINT SOCIO_VENTAS_NIF_fk 
-    FOREIGN KEY(NIF) 
-    REFERENCES SOCIO (NIF),
-
-  CONSTRAINT SOCIO_VENTAS_N_Ventas_fk
-    FOREIGN KEY(N_Ventas)
-    REFERENCES VENTAS(N_Ventas)
-);
-
-CREATE TABLE SOCIO_ENTREGA (
-  NIF VARCHAR(9) NOT NULL,
-  N_Entrega int NOT NULL,
-  
-  CONSTRAINT SOCIO_ENTREGA_pk PRIMARY KEY(NIF,N_Entrega),
-  
-  CONSTRAINT SOCIO_ENTREGA_NIF_fk 
-    FOREIGN KEY(NIF) 
-    REFERENCES SOCIO (NIF),
-
-  CONSTRAINT SOCIO_ENTREGA_N_Entrega_fk
-    FOREIGN KEY(N_Entrega)
-    REFERENCES ENTREGA (N_Entrega)
-);
-
-
-
-CREATE TABLE ENTREGA_PRODUCTO (
-  N_Entrega int NOT NULL,
   Código_aceite VARCHAR(5) NOT NULL,
   Cantidad float NOT NULL,
   
-  CONSTRAINT ENTREGA_PRODUCTO_pk PRIMARY KEY(N_Entrega,Código_aceite),
-  CONSTRAINT ENTREGA_PRODUCTO_Cantidad_ck CHECK (Cantidad>0),
+  CONSTRAINT VENTAS_PRODUCTO_PRODUCTO_pk PRIMARY KEY(N_Ventas,Código_aceite),
+  CONSTRAINT VENTAS_PRODUCTO_Cantidad_ck CHECK (Cantidad>0),
 
-  CONSTRAINT ENTREGA_PRODUCTO_N_Entrega_fk 
-    FOREIGN KEY(N_Entrega) 
-    REFERENCES ENTREGA (N_Entrega),
+  CONSTRAINT VENTAS_PRODUCTO_N_Ventas_fk 
+    FOREIGN KEY(N_Ventas) 
+    REFERENCES VENTAS (N_Ventas),
 
-  CONSTRAINT ENTREGA_PRODUCTO_Código_aceite_fk
+  CONSTRAINT VENTAS_PRODUCTO_Código_aceite_fk
     FOREIGN KEY(Código_aceite)
     REFERENCES PRODUCTO (Código_aceite)
 );
@@ -102,12 +82,10 @@ CREATE TABLE ENTREGA_PRODUCTO (
 -- Datos para insertar en las diferentes tablas. 
 
 INSERT INTO SOCIO VALUE('48769421F', 'Pepe', 'García', 'Pérez', 'C/ Almenas', '23004', 'Jaén', 'Jaén', '*********', '***********@gmail.com');
-INSERT INTO VENTAS VALUE(null, '2024-04-02 12:00');
-INSERT INTO ENTREGA VALUE(null, '2024-04-02 12:00','12', 'Árbol','26','34' );
+INSERT INTO VENTAS VALUE(null, '2024-04-02 12:00','48769421F');
+INSERT INTO ENTREGA VALUE(null, '2024-04-02 12:00','12', 'Árbol','26','34','48769421F' );
 INSERT INTO PRODUCTO VALUE('A87VE','Virgen extra','5.8');
-INSERT INTO SOCIO_VENTAS VALUE('48769421F', '1');
-INSERT INTO SOCIO_ENTREGA VALUE('48769421F', '1');
-INSERT INTO ENTREGA_PRODUCTO VALUE('1', 'A87VE','4');
+INSERT INTO VENTAS_PRODUCTO VALUE("1", 'A87VE','4');
 
 -- Visualizador de la Base de Datos.
 
@@ -115,6 +93,4 @@ SELECT * FROM SOCIO;
 SELECT * FROM VENTAS;
 SELECT * FROM ENTREGA;
 SELECT * FROM PRODUCTO;
-SELECT * FROM SOCIO_VENTAS;
-SELECT * FROM SOCIO_ENTREGA;
-SELECT * FROM ENTREGA_PRODUCTO;
+SELECT * FROM VENTAS_PRODUCTO;
