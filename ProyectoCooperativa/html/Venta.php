3da6db3688daf98f22ca99a5975abd882489ee90
Sylfvain <sureapa243@g.educaand.es>
2024-05-03 19:18:59 +0200

    FIN

--- ProyectoCooperativa/html/Venta.php
+++ ProyectoCooperativa/html/Venta.php
@@ -18,6 +18,7 @@
 $NIF = $_POST['NIF'];
 $FechayhoraVentas = $_POST['fecha_y_hora'];
 
+
 $sentenciaSQL = "INSERT INTO VENTAS (NIF, Fecha_y_hora) VALUES('" . $NIF . "','" . $FechayhoraVentas. "' )"; 
 
 // echo $sentenciaSQL;
@@ -27,7 +28,50 @@ if (!$conn->query($sentenciaSQL)) {
     echo "Falló la inserción de datos en la tabla: (" . $conn->errno . ") " . $conn->error;
 }
 
-// echo "Se ha ejecutado correctamente la inserción de datos";
+// Obtenemos el número de venta
+$sentenciaSQL = "SELECT MAX(N_Ventas) AS ID FROM VENTAS;";
+$statement = $conn->query($sentenciaSQL);
+if ($statement->num_rows > 0) {
+    $result = $statement->fetch_assoc();
+    $id = $result['ID'];
+} else {
+    $id = 0;
+}
+
+
+// Vender producto en primera fila
+$producto = $_POST['TipodeAceite'];
+$cantidad = $_POST['Cantidad1'];
+
+if ($producto != 5 || $cantidad > 0) {
+    $sentenciaSQL = "INSERT INTO VENTAS_PRODUCTO(N_Ventas, Código_aceite, Cantidad) VALUES (" . $id . ",'" . $producto . "'," . $cantidad . ");";
+    echo $sentenciaSQL;
+    if (!$conn->query($sentenciaSQL)) {
+        echo "Falló la inserción de datos en la tabla: (" . $conn->errno . ") " . $conn->error;
+    }
+}
+$producto2 = $_POST['TipodeAceite2'];
+$cantidad2 = $_POST['Cantidad2'];
+
+if ($producto2 != 5 || $cantidad > 0) {
+    $sentenciaSQL = "INSERT INTO VENTAS_PRODUCTO(N_Ventas, Código_aceite, Cantidad) VALUES (" . $id . ",'" . $producto2 . "'," . $cantidad2 . ");";
+    echo $sentenciaSQL;
+    if (!$conn->query($sentenciaSQL)) {
+        echo "Falló la inserción de datos en la tabla: (" . $conn->errno . ") " . $conn->error;
+    }
+}
+$producto3 = $_POST['TipodeAceite3'];
+$cantidad3 = $_POST['Cantidad3'];
+
+if ($producto3 != 5 || $cantidad > 0) {
+    $sentenciaSQL = "INSERT INTO VENTAS_PRODUCTO(N_Ventas, Código_aceite, Cantidad) VALUES (" . $id . ",'" . $producto3 . "'," . $cantidad3 . ");";
+    echo $sentenciaSQL;
+    if (!$conn->query($sentenciaSQL)) {
+        echo "Falló la inserción de datos en la tabla: (" . $conn->errno . ") " . $conn->error;
+    }
+}
+
+echo "Se ha ejecutado correctamente la inserción de datos";
 
     mysqli_close($conn);
     header('Location: ' . $_SERVER['HTTP_REFERER']);
